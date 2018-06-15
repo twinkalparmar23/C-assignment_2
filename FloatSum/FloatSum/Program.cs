ab3b26db24d3485cdd1920bd1de52b59badf3b3e
Twinkal Parmar <twinkal@promactinfo.com>
2018-06-15 18:31:28 +0530

    files added

---
+++ FloatSum/FloatSum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloatSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float X = 0.06f, Y = 5.94f,ans=0.0f;
+
+            Console.WriteLine("X : " + X);
+            Console.WriteLine("Y : " + Y);
+            ans = X + Y;
+
+            Console.WriteLine("\n sum of X and Y is:"+ans);
+
+            bool compareOp = ((X + Y) == ans);
+            Console.WriteLine("\n X+Y == ans :" + compareOp);
+
+            bool compareEqual = X.Equals(Y);
+            Console.WriteLine("X+Y equals sum:" + compareEqual);
+
+            Max(X, Y);
+
+        }
+
+        static void Max<T>(T x,T y) where T:IComparable<T>
+        {
+            if ((x.CompareTo(y) > 0))
+                Console.WriteLine("\nX is greater");
+            if ((y.CompareTo(x)) > 0)
+                Console.WriteLine("\nY is greater");
+
+
+            //int result = x.CompareTo(y);
+            //if (result == 0)
+            //    Console.WriteLine("{0} == {1}", x, y);
+            //else if(result>0)
+            //    Console.WriteLine("{0} > {1}", x, y);
+            //else
+            //    Console.WriteLine("{0} < {1}", x, y);
+        }
+    }
+}
---
+++ Inventory_2/Inventory_2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,count = 0;
+
+            List<string> items = new List<string>();
+
+            items.Add("Tomato");
+            items.Add("Potato");
+            items.Add("ToMato");
+            items.Add("Potato");
+            items.Add("Red Tomato");
+            items.Add("Tomato Catchup");
+            items.Add("Grapes");
+            items.Add("grapes");
+            items.Add("potato");
+            
+            //Food[] list =
+            //{
+            //    new Food("Tomato"),
+            //    new Food("Potato"),
+            //    new Food("ToMato"),
+            //    new Food("Potato"),
+            //    new Food("Red Tomato"),
+            //    new Food("Tomato  Catchup"),
+            //    new Food("Grapes"),
+            //    new Food("grapes"),
+            //    new Food("potato")
+
+            //};
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach(var x in items)
+            {
+                if ((string.Compare(x, "tomato", StringComparison.CurrentCultureIgnoreCase)) == 0)
+                    count = count + 1;
+            }
+
+            Console.WriteLine("\n number of string containing tomato :" + count);
+
+
+            i = items.FindIndex(x => x.Equals("Potato", StringComparison.CurrentCulture));
+            Console.WriteLine("\n Potato found at index:" + (i + 1));
+            
+        }
+    }
+}
